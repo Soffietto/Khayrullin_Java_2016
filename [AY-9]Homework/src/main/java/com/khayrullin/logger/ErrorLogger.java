6a424ee418241322a1bcbbb3fb1eef0981b8d926
Soffietto <azot273@gmail.com>
2016-11-19 17:04:29 +0300

    AY-9: Logger works correctly

--- [AY-9]Homework/src/main/java/com/khayrullin/logger/ErrorLogger.java
+++ [AY-9]Homework/src/main/java/com/khayrullin/logger/ErrorLogger.java
@@ -3,6 +3,6 @@ package com.khayrullin.logger;
 class ErrorLogger extends Logger {
 
     ErrorLogger() {
-        this.setLevel(Level.ERROR.toString() + "|" + Level.INFO);
+        this.setLevel(Level.ERROR.toString());
     }
 }
