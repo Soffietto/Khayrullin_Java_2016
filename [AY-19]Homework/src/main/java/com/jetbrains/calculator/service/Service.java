cd4b16adea192f608029de1edce24a1d6786f4a4
Soffietto <azot273@gmail.com>
2016-12-10 18:30:05 +0300

    [AY-19]Homework: Service methods replaced, variable name chanched, pattern set to static

--- [AY-19]Homework/src/main/java/com/jetbrains/calculator/service/Service.java
+++ [AY-19]Homework/src/main/java/com/jetbrains/calculator/service/Service.java
@@ -7,41 +7,41 @@ import java.util.regex.Pattern;
 
 public class Service {
     private Calculator calculator;
-    private double Number1;
-    private double Number2;
+    private double variableA;
+    private double variableB;
     private char operation;
-    private final Pattern CALCULATOR_PATTERN = Pattern.compile("/calculator/(?<variableA>\\d+(\\d)*)(?<operation>(\\+|-|/|\\*))(?<variableB>\\d+(\\d)*)");
+    private static final Pattern CALCULATOR_PATTERN = Pattern.compile("/calculator/(?<variableA>\\d+(\\d)*)(?<operation>(\\+|-|/|\\*))(?<variableB>\\d+(\\d)*)");
     private Matcher m = null;
     private Double result;
-
-    private void parseCalculation(String message) {
-        m = CALCULATOR_PATTERN.matcher(message);
-        if (m.matches()) {
-            this.Number1 = Double.parseDouble(m.group("variableA"));
-            this.Number2 = Double.parseDouble(m.group("variableB"));
-            this.operation = m.group("operation").charAt(0);
-        }else {
-            throw new WrongParserException("Wrong expression");
-        }
-    }
 
     public double calculate(String message) {
         calculator = new Calculator();
         parseCalculation(message);
         switch (operation) {
             case '+':
-                result = calculator.sum(Number1, Number2);
+                result = calculator.sum(variableA, variableB);
                 break;
             case '-':
-                result = calculator.sub(Number1, Number2);
+                result = calculator.sub(variableA, variableB);
                 break;
             case '/':
-                result = calculator.div(Number1, Number2);
+                result = calculator.div(variableA, variableB);
                 break;
             case '*':
-                result = calculator.mult(Number1, Number2);
+                result = calculator.mult(variableA, variableB);
                 break;
         }
         return result;
+    }
+
+    private void parseCalculation(String message) {
+        m = CALCULATOR_PATTERN.matcher(message);
+        if (m.matches()) {
+            this.variableA = Double.parseDouble(m.group("variableA"));
+            this.variableB = Double.parseDouble(m.group("variableB"));
+            this.operation = m.group("operation").charAt(0);
+        }else {
+            throw new WrongParserException("Wrong expression");
+        }
     }
 }
